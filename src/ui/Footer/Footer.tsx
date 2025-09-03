f9248c3fb76cc0e433a0f5c52a8fed51598d7f2e
Vladislav Kaminskiy <enotvodkin@gmail.com>
2025-09-03 13:58:26 +0200

    refactor: footer styles
    add hover interactive opacity placeholder
    add hover interactive decorated line placeholder
    refactor global styles

--- src/ui/Footer/Footer.tsx
+++ src/ui/Footer/Footer.tsx
@@ -6,10 +6,12 @@ export const Footer = () => (
     <div className={`container ${styles.content}`}>
       <div className={styles.wrapper}>
         <div className={styles.social}>
-          <h4 className={styles.footerTitle}>Социальные сети</h4>
+          <h4 className={styles.footerTitle}>Социальные&nbsp;сети</h4>
           <nav className={styles.socialList}>
             {Object.entries(socialLinks).map(([network, link]) => (
-              <a href={link as string}>{network}</a>
+              <a href={link as string} key={network}>
+                {network}
+              </a>
             ))}
           </nav>
         </div>
@@ -17,14 +19,19 @@ export const Footer = () => (
       <img src="/logo-cropped.webp" className={styles.logo}></img>
       <div className={styles.wrapper}>
         <div className={styles.social}>
-          <h4 className={styles.footerTitle}>Часы работы</h4>
+          <h4 className={styles.footerTitle}>Часы&nbsp;работы</h4>
           <div className={styles.socialList}>
-            Понедельник — Пятница <p>09:00 - 20:00</p>
+            Понедельник — Пятница <p>09:00 — 20:00</p>
           </div>
         </div>
       </div>
+      <address className={styles.contacts}>
+        <a href="mailto:polonskaya.remont@gmail.com">
+          polonskaya.remont@gmail.com
+        </a>
+        <a href="tel:+79013903946">+7 (901) 390-39-46</a>
+      </address>
     </div>
-    <address>polonskaya.remont@gmail.com&ntsp;+ 7 (901) 390-39-46</address>
-    Калининград
+    <p className="city">Калининград 2025</p>
   </footer>
 );
